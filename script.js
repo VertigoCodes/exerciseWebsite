647074cdf1e46d41eb2f65e2c763e4cbf0b45457
VertigoCodes <t.yuvaraja01@gmail.com>
2023-12-29 13:25:43 +0530

    updated script

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-alert("Hello, World!");
-
-
+alert("Vaazgha Vayagam, Vaazgha Valamudan");
 
 // Replace 'YOUR_PUBLISHED_SHEET_LINK' with the link you copied
 const sheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQCyNsRhvPH_vzBcIpWjehnLM46qZEBHupoXQH9t-bsKaSFcXPkty4aNqA1t-F0F0PYyKgLkHJg1_RI/pub?gid=283396415&single=true&output=csv';
