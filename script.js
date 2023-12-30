b2dd7fc436f6872f3a53c3d491f71750c95b58f2
VertigoCodes <t.yuvaraja01@gmail.com>
2023-12-30 19:13:23 +0530

    jquery added

--- script.js
+++ script.js
@@ -4,16 +4,14 @@ alert("Vaazgha Vayagam, Vaazgha Valamudan");
 const sheetURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQCyNsRhvPH_vzBcIpWjehnLM46qZEBHupoXQH9t-bsKaSFcXPkty4aNqA1t-F0F0PYyKgLkHJg1_RI/pub?gid=283396415&single=true&output=csv';
 
 // Fetch data from the published Google Sheet
-async function fetchData() {
-  try {
-    const response = await fetch(sheetURL);
-    const data = await response.text();
-
+function fetchData() {
+  $.get(sheetURL, function (data) {
     // Process the data as needed
     console.log('Data from Google Sheet:', data);
-  } catch (error) {
+  })
+  .fail(function (error) {
     console.error('Error fetching data:', error);
-  }
+  });
 }
 
 // Call the function to fetch data
